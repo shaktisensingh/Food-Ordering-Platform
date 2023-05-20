e335aabb04b5c1b2dbb33af4127a16e0340ae380
shaktisensingh <38095949+shaktisensingh@users.noreply.github.com>
2023-05-21 01:11:07 +0530

    first commit

---
+++ src/main/java/dxc/casestudy/fop/FoodOrderingPlatformApplication.java
@@ -0,0 +1,13 @@
+package dxc.casestudy.fop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dxc/casestudy/fop/FoodOrderingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dxc.casestudy.fop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
